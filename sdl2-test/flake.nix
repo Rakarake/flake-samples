e4622a16fde26c4b2645b01efea3af90cf643735
Rakarake <rak@rakarake.xyz>
2024-02-05 04:19:47 +0100

    SDL2 sample

---
+++ sdl2-test/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Sample project";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gcc
+            pkg-config
+            SDL2
+          ];
+        };
+      }
+    );
+}
